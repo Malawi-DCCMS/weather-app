5352a6d45582bf4af6167845f67d4e53a9df9258
Håvard Futsæter <havardf@met.no>
2023-04-19 12:02:35 +0200

    Access gps location and print location.

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import GPSLocation from './components/GPSLocation';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <GPSLocation />
+    </View>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
