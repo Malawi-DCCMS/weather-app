1f01ba10df4de43463dfdafd30e1f89ffb50ed12
danielmwakanema <danielmwakanema95@gmail.com>
2023-05-24 05:41:29 +0200

    refactor(app): add autocomplete context

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {AutocompleteDropdownContextProvider} from 'react-native-autocomplete-dropdown';
 import Home from './components/Home';
 import Detailed from './components/forecast/Detailed';
 
@@ -8,12 +9,18 @@ const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} options={{title: 'Home'}} />
-        <Stack.Screen name="7 days forecast" component={Detailed} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AutocompleteDropdownContextProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{title: 'Home'}}
+          />
+          <Stack.Screen name="7 days forecast" component={Detailed} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AutocompleteDropdownContextProvider>
   );
 }
 
