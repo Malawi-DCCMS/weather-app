40c5568de2fca792022b0ecdc4ff6f5e90d86323
danielmwakanema <danielmwakanema95@gmail.com>
2023-05-24 05:43:11 +0200

    feat(app): add geonames service

---
+++ src/services/geonames.service.ts
@@ -0,0 +1,31 @@
+import {GEONAMES} from '../../assets/geonames';
+
+function placeFromGeoname(value: string): Geoname | undefined {
+  const fields = value.split(/\t/);
+  const lat = parseFloat(fields[4]);
+  const long = parseFloat(fields[5]);
+  if (isNaN(lat) || isNaN(long)) {
+    return;
+  }
+
+  return {
+    lat: parseFloat(fields[4]),
+    long: parseFloat(fields[5]),
+    name: fields[1],
+  };
+}
+
+function parseGeonames(geonames: string): Array<string> {
+  return geonames.split(/\r?\n/);
+}
+
+export function getGeonames(): Array<Geoname> {
+  const rawPlaces = parseGeonames(GEONAMES);
+  return rawPlaces.reduce((acc: Array<Geoname>, val) => {
+    const place = placeFromGeoname(val);
+    if (place) {
+      acc.push(place);
+    }
+    return acc;
+  }, []);
+}
