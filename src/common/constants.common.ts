8656f36c17c979f881da27e5f16ca263316a8965
danielmwakanema <danielmwakanema95@gmail.com>
2023-10-26 10:50:17 +0200

    chore(consts): add weather warnings

---
+++ src/common/constants.common.ts
@@ -0,0 +1,10 @@
+import weatherIcons from '../../constants/weathericons';
+
+export const DEFAULT_PLACE: Place = {
+  name: 'Blantyre',
+  position: {lat: -15.786111, long: 35.005833},
+};
+
+export const WEATHER_WARNINGS = {
+  yellow: weatherIcons['icon_warning_wind_yellow'],
+};
