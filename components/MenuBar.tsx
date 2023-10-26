c4d24d8beeb72942ffb7d2639566554825379b99
danielmwakanema <danielmwakanema95@gmail.com>
2023-10-26 10:50:31 +0200

    feat(menubar): add menu bar

--- components/MenuBar.tsx
+++ components/MenuBar.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
-import { Appbar } from 'react-native-paper';
+import { Appbar, Icon } from 'react-native-paper';
 
-const MenuBar = () => (
-  <Appbar.Header>
-    <Appbar.BackAction onPress={() => {}} />
-    <Appbar.Content title="Title" />
-    <Appbar.Action icon="magnify" onPress={() => {}} />
-    <Appbar.Action icon="menu" onPress={() => {}} />
+import { WEATHER_WARNINGS } from '../src/common';
+
+type MenuBarProps = {location: string};
+
+const MenuBar = (props: MenuBarProps) => {
+  const tooLong = props.location.length > 15;
+  const fmtLocation = tooLong ? `${props.location?.slice(0, 15)}...` : props.location;
+  return (
+    <Appbar.Header style={{backgroundColor: '#E4E5D6', paddingTop: 2}}>
+    <Appbar.Content title={fmtLocation} style={{flexDirection: 'row', paddingLeft: 13}}/>
+    <Appbar.Action icon={() => <Icon size={24} source={WEATHER_WARNINGS.yellow} />} onPress={() => {}}/>
+    <Appbar.Action icon="magnify" size={24} onPress={() => {}} />
+    <Appbar.Action icon="menu" size={24} onPress={() => {}} />
   </Appbar.Header>
-);
+  );
+}
 
 export default MenuBar;
