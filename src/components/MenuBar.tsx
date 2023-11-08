7efcd60aecb242f4e328a011210cb3adc8f73d37
Daniel Mwakanema <danielmwakanema95@gmail.com>
2023-11-08 16:49:35 +0200

    refactor(menubar): change header font

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -10,7 +10,7 @@ const MenuBar = (props: MenuBarProps) => {
   const fmtLocation = tooLong ? `${props.location?.slice(0, 15)}...` : props.location;
   return (
     <Appbar.Header style={{ backgroundColor: '#E4E5D6', marginTop: -20, paddingBottom: 4 }}>
-      <Appbar.Content title={fmtLocation} titleStyle={{fontSize: 20, fontFamily: 'Noto Sans'}} style={{ flexDirection: 'row', paddingLeft: 10, paddingTop: 12 }} />
+      <Appbar.Content title={fmtLocation} titleStyle={{fontSize: 28, fontFamily: 'NotoSans-Regular'}} style={{ flexDirection: 'row', paddingLeft: 10, paddingTop: 12 }} />
       <Appbar.Action icon={() => <Icon size={24} source={WEATHER_WARNINGS.yellow} />} onPress={() => { }} style={{marginLeft: -15, paddingTop: 12}} />
       <Appbar.Action icon="magnify" size={24} onPress={() => { }} style={{marginRight: -10, paddingTop: 12}} />
       <Appbar.Action icon="menu" size={24} onPress={() => { }} style={{marginRight: 5, paddingTop: 12}} />
