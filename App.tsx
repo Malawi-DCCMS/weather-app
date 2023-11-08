954d2036b26d84539cfa50a67ff5afe986109874
Daniel Mwakanema <danielmwakanema95@gmail.com>
2023-11-08 16:49:50 +0200

    refactor(app): default to home screen

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 import HourlyScreen from './src/screens/hourly.screen';
+import HomeScreen from './src/screens/home.screen';
 
 function App(): JSX.Element {
   return (
     <SafeAreaView>
-      <HourlyScreen />
+      <HomeScreen />
     </SafeAreaView>
   );
 }
