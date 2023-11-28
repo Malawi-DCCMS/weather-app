9daba27ac6a782ca0c1590844e3a43b478e7bcff
Daniel Mwakanema <danielmwakanema95@gmail.com>
2023-11-28 15:27:48 +0200

    refactor(app): add more screens

--- App.tsx
+++ App.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
-import { SafeAreaView } from 'react-native-safe-area-context';
 import HourScreen from './src/screens/hour.screen';
 import MainScreen from './src/screens/main.screen';
+import NoLocationScreen from './src/screens/no-location.screen';
+import SearchScreen from './src/screens/search.screen';
+import { SCREENS } from './src/constants/screens.constant';
+
+const Drawer = createDrawerNavigator();
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <MainScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName={SCREENS.home} screenOptions={{
+        headerShown: false,
+        drawerStyle: {
+          backgroundColor: '#E4E5D6',
+          width: 240,
+        },
+      }}>
+        <Drawer.Screen name={SCREENS.home} component={MainScreen} />
+        <Drawer.Screen name={SCREENS.nolocation} component={NoLocationScreen} options={{drawerItemStyle: { height: 0 }}} />
+        <Drawer.Screen name={SCREENS.hourly} component={HourScreen} options={{drawerItemStyle: { height: 0 }}} />
+        <Drawer.Screen name={SCREENS.search} component={SearchScreen} options={{drawerItemStyle: { height: 0 }}} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
