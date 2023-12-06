936db34829dbee702c67ca3f10d882ef20afcd58
Daniel Mwakanema <danielmwakanema95@gmail.com>
2023-12-06 15:22:54 +0200

    refactor(app): use stack navigator

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
+import 'react-native-reanimated'
+import 'react-native-gesture-handler'
 import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 
 import HourScreen from './src/screens/hour.screen';
@@ -8,18 +9,15 @@ import NoLocationScreen from './src/screens/no-location.screen';
 import SearchScreen from './src/screens/search.screen';
 import { SCREENS } from './src/constants/screens.constant';
 import FeedbackScreen from './src/screens/feedback.screen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Drawer = createDrawerNavigator();
+const Drawer = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName={SCREENS.home} screenOptions={{
-        headerShown: false,
-        drawerStyle: {
-          backgroundColor: '#E4E5D6',
-          width: 240,
-        },
+        headerShown: false
       }}>
         <Drawer.Screen name={SCREENS.home} component={MainScreen} />
         <Drawer.Screen name={SCREENS.feedback} component={FeedbackScreen} />
