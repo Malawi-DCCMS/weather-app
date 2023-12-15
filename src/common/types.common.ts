17d70589dcaa888ca3ceedeeaf2f440c64e9e63b
Daniel Mwakanema <danielmwakanema95@gmail.com>
2023-12-15 05:13:00 +0200

    chore(types): add new screen name

--- src/common/types.common.ts
+++ src/common/types.common.ts
@@ -1,7 +1,8 @@
-import { WeatherForecast } from "../utils/locationforecast";
+import { ForecastTimestep, WeatherForecast } from "../utils/locationforecast";
 
 export type RootDrawerParamList = {
-  Hourly: { name: string, forecast: WeatherForecast };
+  Hourly: { name: string, forecast: WeatherForecast, title: string };
+  Day: { name: string, forecast: Array<ForecastTimestep> };
   NoLocation: undefined;
   Search: undefined;
   Home: undefined;
