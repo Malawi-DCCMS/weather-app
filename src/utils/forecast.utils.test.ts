4dd7bd452dd35a71e31d30aab6ebfade2ee45d85
Vegard Bønes <vegardb@met.no>
2024-01-03 10:47:51 +0100

    Make today's forecast more correct

---
+++ src/utils/forecast.utils.test.ts
@@ -0,0 +1,67 @@
+import moment from "moment";
+import { getForecastSymbolAtTime } from "./forecast.utils";
+import { ForecastTimestep } from './locationforecast';
+
+
+describe("select correct weather symbol for today's forecast", () => {
+    const forecast = sampleForecast()
+
+    test('early night', () => {
+        const symbol = getForecastSymbolAtTime(moment.utc('2023-12-29T02:00:00'), forecast)
+        expect(symbol).toBe('12h@4')
+    })
+
+    test('early day', () => {
+        const symbol = getForecastSymbolAtTime(moment.utc('2023-12-29T06:00:00'), forecast)
+        expect(symbol).toBe('6h@10')
+    })
+
+    test('late day', () => {
+        const symbol = getForecastSymbolAtTime(moment.utc('2023-12-29T17:00:00'), forecast)
+        expect(symbol).toBe('1h@17')
+    })
+
+    test('midnight', () => {
+        const symbol = getForecastSymbolAtTime(moment.utc('2023-12-29T00:00:00'), forecast)
+        expect(symbol).toBe('12h@4')
+    })
+})
+
+/**
+ * Creates a sample forecast for testing. 
+ * Each timestep has a symbol code for each time range, with format 6h@3, meaning next_6_hours at 03:00.
+ * 
+ * @returns 
+ */
+function sampleForecast(): Array<ForecastTimestep> {
+    let ret: Array<ForecastTimestep> = []
+
+    for (let i = 0; i < 24; i++) {
+        const doc = {
+            time: `2023-12-29T${('00' + i).slice(-2)}:00:00Z`,
+            data: {
+                instant: {
+                    details: {}
+                },
+                next_1_hours: {
+                    summary: {
+                        symbol_code: `1h@${i}`
+                    }
+                },
+                next_6_hours: {
+                    summary: {
+                        symbol_code: `6h@${i}`
+                    }
+                },
+                next_12_hours: {
+                    summary: {
+                        symbol_code: `12h@${i}`
+                    }
+                }
+            }
+        }
+        ret.push(doc)
+    }
+
+    return ret
+}
--- src/utils/forecast.utils.ts
+++ src/utils/forecast.utils.ts
@@ -1,5 +1,24 @@
 import { FORECAST_DESCRIPTIONS } from '../constants/forecast-descriptions.constant';
+import moment from 'moment';
+import { ForecastTimestep } from './locationforecast';
+
 
 export function getForecastDescription(key: string): string {
   return FORECAST_DESCRIPTIONS[key];
+}
+
+export function getForecastSymbolAtTime(t: moment.Moment, timesteps: Array<ForecastTimestep>): string | undefined {
+  const day = t.format('YYYY-MM-DD');
+
+  if (t.isBefore(moment(`${day} 04:00:00Z`))) {
+    const timestep = timesteps.find(timestep => timestep.time == `${day}T04:00:00Z`);
+    return timestep?.data.next_12_hours?.summary?.symbol_code;
+  }
+  if (t.isBefore(moment(`${day} 10:00:00Z`))) {
+    const timestep = timesteps.find(timestep => timestep.time == `${day}T10:00:00Z`);
+    return timestep?.data.next_6_hours?.summary?.symbol_code;
+  }
+
+  const timestep = timesteps.find(timestep => timestep.time == t.format('YYYY-MM-DDTHH') + ':00:00Z');
+  return timestep?.data.next_1_hours?.summary?.symbol_code;
 }
