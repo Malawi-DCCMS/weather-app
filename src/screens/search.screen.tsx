33d2f4f0b1020ffa6fa8c6dfd2d03e9c42ed0a3a
Daniel Mwakanema <danielmwakanema95@gmail.com>
2024-03-01 11:22:42 +0200

    refactor(search-screen): make glassmorphy
    - use new background
    - fix prop typing

--- src/screens/search.screen.tsx
+++ src/screens/search.screen.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { ImageBackground, StyleSheet, View } from 'react-native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { ParamListBase } from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { useDispatch, useSelector } from 'react-redux';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { AutocompleteDropdownContextProvider } from 'react-native-autocomplete-dropdown';
 
-import appBackground from '../../assets/app-bg-normal.png';
+import appBackground from '../../assets/MOBILE-craig-manners-dyWHuFsdfIo-unsplash.png';
 import AppBar from '../components/AppBar';
 import { Search } from '../components/Search';
 import { SCREENS } from '../constants/screens.constant';
 import { AppDispatch, RootState } from '../store';
 import { setName, setLat, setLon } from '../store/location.slice';
+import { RootDrawerParamList } from '../common';
 
-type ScreenProps = {
-  navigation: NativeStackNavigationProp<ParamListBase>;
-}
+type ScreenProps = NativeStackScreenProps<RootDrawerParamList, 'Search'>;
 const SearchScreen = ({ navigation }: ScreenProps) => {
   const dispatch = useDispatch<AppDispatch>();
   const { name } = useSelector((state: RootState) => state.location);
@@ -33,7 +31,7 @@ const SearchScreen = ({ navigation }: ScreenProps) => {
                   dispatch(setName(place.name));
                   dispatch(setLat(place.lat));
                   dispatch(setLon(place.long));
-                  navigation.navigate(SCREENS.home);
+                  navigation.navigate(SCREENS.Home);
                 }
               }
             />
