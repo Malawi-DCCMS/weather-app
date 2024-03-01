86a68a3b7a4adb527d704fa956be6c23bd719b01
Daniel Mwakanema <danielmwakanema95@gmail.com>
2024-03-01 11:21:10 +0200

    refactor(hourly-table): make glassmorphy

--- src/components/HourlyTable.tsx
+++ src/components/HourlyTable.tsx
@@ -5,6 +5,7 @@ import { DateTime } from "luxon";
 
 import weatherIcons from '../constants/weathericons.constant';
 import { DaySummary } from '../utils/weatherData';
+import { GlassView } from '../components/GlassView';
 
 type HourlyTableProps = {
   title: string;
@@ -17,29 +18,30 @@ type HourlyTableProps = {
  */
 function HourlyTable(props: HourlyTableProps): JSX.Element {
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>{props.title}</Text>
-      <DataTable style={styles.table}>
-        <DataTable.Header>
-          <DataTable.Title>Time</DataTable.Title>
-          <DataTable.Title><Text></Text></DataTable.Title>
-          <DataTable.Title numeric>Temp C&deg;</DataTable.Title>
-          <DataTable.Title numeric>Rain mm</DataTable.Title>
-          <DataTable.Title numeric>Wind km/h</DataTable.Title>
-        </DataTable.Header>
-        <ScrollView snapToStart={false}>
-          {props.daySummary.steps.map((step) => (
-            <DataTable.Row key={step.time.toISO()}>
-              <DataTable.Cell>{step.time.toLocaleString(DateTime.TIME_24_SIMPLE)}</DataTable.Cell>
-              <DataTable.Cell><Icon source={weatherIcons[step.weatherSymbol()]} size={25} /></DataTable.Cell>
-              <DataTable.Cell numeric>{step.temperature}&deg;</DataTable.Cell>
-              <DataTable.Cell numeric>{step.precipitation()}</DataTable.Cell>
-              <DataTable.Cell numeric>{step.windSpeed}</DataTable.Cell>
-            </DataTable.Row>
-          ))}
-        </ScrollView>
-      </DataTable>
-    </View>
+    <GlassView glassStyle={styles.container} containerStyle={styles.container} blurStyle={{ blurAmount: 8, blurType: 'light' }}>
+      <View style={styles.opacity}>
+        <DataTable style={styles.table}>
+          <DataTable.Header>
+            <DataTable.Title><Text style={styles.whiteHeader}>Time</Text></DataTable.Title>
+            <DataTable.Title><Text></Text></DataTable.Title>
+            <DataTable.Title numeric><Text style={styles.whiteHeader}>Temp C&deg;</Text></DataTable.Title>
+            <DataTable.Title numeric><Text style={styles.whiteHeader}>Rain mm</Text></DataTable.Title>
+            <DataTable.Title numeric><Text style={styles.whiteHeader}>Wind km/h</Text></DataTable.Title>
+          </DataTable.Header>
+          <ScrollView snapToStart={false}>
+            {props.daySummary.steps.map((step) => (
+              <DataTable.Row key={step.time.toISO()}>
+                <DataTable.Cell><Text style={styles.whiteText}>{step.time.toLocaleString({ hour: '2-digit' })}</Text></DataTable.Cell>
+                <DataTable.Cell><Icon source={weatherIcons[step.weatherSymbol()]} size={36} /></DataTable.Cell>
+                <DataTable.Cell numeric><Text style={styles.whiteText}>{step.temperature}&deg;</Text></DataTable.Cell>
+                <DataTable.Cell numeric><Text style={styles.whiteText}>{step.precipitation()}</Text></DataTable.Cell>
+                <DataTable.Cell numeric><Text style={styles.whiteText}>{step.windSpeed}</Text></DataTable.Cell>
+              </DataTable.Row>
+            ))}
+          </ScrollView>
+        </DataTable>
+      </View>
+    </GlassView>
   );
 };
 
@@ -48,19 +50,25 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     flex: 1,
   },
-  header: {
-    fontSize: 20,
-    fontFamily: 'Rajdhani-Regular',
-    paddingRight: 20,
-    paddingLeft: 20,
-    paddingTop: 20,
-  },
   table: {
-    paddingLeft: 12,
-    paddingRight: 12,
+    paddingLeft: 24,
+    paddingRight: 20,
+    paddingTop: 40,
     marginTop: 5,
     flex: 1
-  }
+  },
+  opacity: {
+    flexDirection: 'column',
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, .1)',
+  },
+  whiteHeader: {
+    color: 'white',
+  },
+  whiteText: {
+    color: 'white',
+    fontSize: 16,
+  },
 });
 
 export default HourlyTable;
