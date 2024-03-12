9af27c1246435bbed0b77414ffd5e4d9ef6a1115
Daniel Mwakanema <danielmwakanema95@gmail.com>
2024-03-12 08:02:16 +0200

    refactor(hourly): drop unused ref

--- src/components/HourlyTable.tsx
+++ src/components/HourlyTable.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { DataTable, Icon } from 'react-native-paper';
 import { DateTime } from "luxon";
 
