96ee498b6f99fb43490a7b91de80a322513796a2
Daniel Mwakanema <danielmwakanema95@gmail.com>
2024-03-12 07:57:34 +0200

    refactor(appbar): migrate to blurview
    - migrated blur effect provider to react-native-community/blur

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Icon, Menu } from 'react-native-paper';
 import { ParamListBase, RouteProp, useRoute } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { BlurView } from '@react-native-community/blur';
 
 import { WEATHER_WARNINGS } from '../common';
 import { SCREENS } from '../constants/screens.constant';
-import backAndroid from '../../assets/icons8-back-android.png';
-import backIOS from '../../assets/icons8-back-ios.png';
-import { GlassView } from '../components/GlassView';
+import backArrow from '../../assets/icons8-back-100_2.png';
 
 type AppBarProps = {
   location: string,
@@ -17,44 +16,44 @@ type AppBarProps = {
 };
 
 const AppBar = (props: AppBarProps) => {
-  const tooLong = props.location.length > 12;
+  const tooLong = props.location.length > 15;
   const fmtLocation = tooLong ? `${props.location?.slice(0, 10)}...` : props.location;
 
   const showSearch = useRoute().name !== SCREENS.Search;
   const [visible, setVisible] = React.useState(false);
   const openMenu = () => setVisible(true);
   const closeMenu = () => setVisible(false);
-  const backArrow = Platform.OS === 'ios' ? backIOS : backAndroid;
 
   return (
-      <GlassView style={styles.appBar} glassStyle={styles.appBar}>
-        <View style={styles.appTitleContainer}>
-          {props.navigation.canGoBack() && <TouchableOpacity onPress={() => props.navigation.goBack()} style={{ paddingRight: 12 }}><Icon size={24} color='white' source={backArrow} /></TouchableOpacity>}
-          <Text style={styles.appTitle}>{fmtLocation}</Text>
-          <TouchableOpacity style={styles.weatherWarning}><Icon size={33} source={WEATHER_WARNINGS.yellow} /></TouchableOpacity>
-        </View>
+    <View style={styles.appBar}>
+      <View style={styles.appTitleContainer}>
+        {props.navigation.canGoBack() && <TouchableOpacity onPress={() => props.navigation.goBack()} style={{ paddingRight: 12 }}><Icon size={24} color='white' source={backArrow} /></TouchableOpacity>}
+        <Text style={styles.appTitle}>{fmtLocation}</Text>
+        <TouchableOpacity style={styles.weatherWarning}><Icon size={33} source={WEATHER_WARNINGS.yellow} /></TouchableOpacity>
+      </View>
 
-        <View style={styles.appNav}>
-          {showSearch && <TouchableOpacity style={styles.items} onPress={() => props.navigation.navigate(SCREENS.Search)}><Icon size={24} color='white' source="magnify" /></TouchableOpacity>}
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'center',
-            }}>
-            <Menu
-              visible={visible}
-              onDismiss={closeMenu} anchor={<TouchableOpacity onPress={() => openMenu()}><Icon size={24} color='white' source={visible ? "close" : "menu"} /></TouchableOpacity>}
-              contentStyle={{ backgroundColor: 'rgba(217, 217, 217, 0.7)', marginTop: 25 }}
-            >
-              <Menu.Item onPress={() => { closeMenu(); }} style={styles.menuItem} titleStyle={styles.menuItemTitle} title="Favourites" />
-              <Menu.Item onPress={() => { closeMenu(); props.navigation.navigate(SCREENS.AboutUs); }} style={styles.menuItem} titleStyle={styles.menuItemTitle} title="About the app" />
-              <Menu.Item onPress={() => { closeMenu(); }} style={styles.menuItem} titleStyle={styles.menuItemTitle} title="Settings" />
-              <Menu.Item onPress={() => { closeMenu(); props.navigation.navigate(SCREENS.Feedback); }} style={styles.menuItem} titleStyle={styles.menuItemTitle} title="Give feedback" />
-              <Menu.Item onPress={() => { closeMenu(); }} style={styles.menuItem} titleStyle={styles.menuItemTitle} title="Help" />
-            </Menu>
-          </View>
+      <View style={styles.appNav}>
+        {showSearch && <TouchableOpacity style={styles.items} onPress={() => props.navigation.navigate(SCREENS.Search)}><Icon size={24} color='white' source="magnify" /></TouchableOpacity>}
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}>
+          <Menu
+            visible={visible}
+            onDismiss={closeMenu} anchor={<TouchableOpacity onPress={() => openMenu()}><Icon size={24} color='white' source={visible ? "close" : "menu"} /></TouchableOpacity>}
+            contentStyle={{ backgroundColor: 'rgba(217, 217, 217, 0.7)', marginTop: 25 }}
+          >
+            <Menu.Item onPress={() => { closeMenu(); }} style={styles.menuItem} titleStyle={styles.menuItemTitle} title="Favourites" />
+            <Menu.Item onPress={() => { closeMenu(); props.navigation.navigate(SCREENS.AboutUs); }} style={styles.menuItem} titleStyle={styles.menuItemTitle} title="About the app" />
+            <Menu.Item onPress={() => { closeMenu(); }} style={styles.menuItem} titleStyle={styles.menuItemTitle} title="Settings" />
+            <Menu.Item onPress={() => { closeMenu(); props.navigation.navigate(SCREENS.Feedback); }} style={styles.menuItem} titleStyle={styles.menuItemTitle} title="Give feedback" />
+            <Menu.Item onPress={() => { closeMenu(); }} style={styles.menuItem} titleStyle={styles.menuItemTitle} title="Help" />
+          </Menu>
         </View>
-      </GlassView>
+      </View>
+      <BlurView blurAmount={50} blurType='light' style={styles.blurBar} />
+    </View>
   );
 }
 
@@ -63,19 +62,32 @@ export default AppBar;
 const styles = StyleSheet.create({
   appBar: {
     flexDirection: 'row',
+  },
+  blurBar: {
+    flex: 1,
     paddingRight: 20,
     paddingLeft: 20,
     paddingTop: 12,
     paddingBottom: 12,
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    height: '100%',
+    zIndex: 0,
   },
   appTitleContainer: {
+    paddingRight: 13,
+    paddingLeft: 13,
+    paddingTop: 17,
+    paddingBottom: 17,
     flex: 7,
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
+    zIndex: 1,
   },
   appTitle: {
-    fontSize: 28,
+    fontSize: 20,
     fontFamily: 'NotoSans-Regular',
     color: 'white',
     marginRight: 5,
@@ -85,6 +97,11 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-end',
     alignItems: 'center',
+    zIndex: 1,
+    paddingRight: 20,
+    paddingLeft: 20,
+    paddingTop: 12,
+    paddingBottom: 12,
   },
   items: {
     paddingRight: 15,
