bca9f56a7a76bcb529543d8f41b94da0b8207db7
Daniel Mwakanema <danielmwakanema95@gmail.com>
2024-03-15 15:12:38 +0200

    refactor(app): switch to drawer navigator

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import 'react-native-reanimated'
 import 'react-native-gesture-handler'
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import HourScreen from './src/screens/hour.screen';
 import MainScreen from './src/screens/main.screen';
@@ -14,21 +14,21 @@ import { SCREENS } from './src/constants/screens.constant';
 import { RootDrawerParamList } from './src/common';
 
 
-const Stack = createStackNavigator<RootDrawerParamList>();
+const Drawer = createDrawerNavigator<RootDrawerParamList>();
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={SCREENS.Home} screenOptions={{
+      <Drawer.Navigator initialRouteName={SCREENS.Home} screenOptions={{
         headerShown: false
       }}>
-        <Stack.Screen name={SCREENS.Home} options={TransitionPresets.SlideFromRightIOS} component={MainScreen} />
-        <Stack.Screen name={SCREENS.Feedback} options={TransitionPresets.SlideFromRightIOS} component={FeedbackScreen} />
-        <Stack.Screen name={SCREENS.NoLocation} options={TransitionPresets.SlideFromRightIOS} component={NoLocationScreen} />
-        <Stack.Screen name={SCREENS.Hourly} options={TransitionPresets.SlideFromRightIOS} component={HourScreen} />
-        <Stack.Screen name={SCREENS.Search} options={TransitionPresets.SlideFromRightIOS} component={SearchScreen} />
-        <Stack.Screen name={SCREENS.AboutUs} options={TransitionPresets.SlideFromRightIOS} component={AboutUsScreen} />
-      </Stack.Navigator>
+        <Drawer.Screen name={SCREENS.Home} component={MainScreen} />
+        <Drawer.Screen name={SCREENS.Feedback} component={FeedbackScreen} />
+        <Drawer.Screen name={SCREENS.NoLocation} component={NoLocationScreen} />
+        <Drawer.Screen name={SCREENS.Hourly} component={HourScreen} />
+        <Drawer.Screen name={SCREENS.Search} component={SearchScreen} />
+        <Drawer.Screen name={SCREENS.AboutUs} component={AboutUsScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
