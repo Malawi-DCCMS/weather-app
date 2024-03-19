afa8498d3142d2aa9b82629a2667f74f7eae768d
Håvard Futsæter <havardf@met.no>
2024-03-19 13:45:25 +0100

    Refactor how to get geonames.

--- src/services/geonames.service.ts
+++ src/services/geonames.service.ts
@@ -19,13 +19,25 @@ function parseGeonames(geonames: string): Array<string> {
   return geonames.split(/\r?\n/);
 }
 
-export function getGeonames(): Array<Geoname> {
+export function getGeonames(): Record<string, Geoname> {
   const rawPlaces = parseGeonames(GEONAMESUNIQUE);
-  return rawPlaces.reduce((acc: Array<Geoname>, val) => {
-    const place = placeFromGeoname(val);
-    if (place) {
-      acc.push(place);
-    }
-    return acc;
-  }, []);
+
+  return rawPlaces.reduce(
+    (acc: Record<string, Geoname>, val) => {
+      const place = placeFromGeoname(val);
+      if (place) {
+        ((acc[place.name] = place), acc)
+      }
+      return acc
+    },
+    {},
+  );
+
+  // return rawPlaces.reduce((acc: Array<Geoname>, val) => {
+  //   const place = placeFromGeoname(val);
+  //   if (place) {
+  //     acc.push(place);
+  //   }
+  //   return acc;
+  // }, []);
 }
