513baab9336a4b3fe6577b16a3346160897ed8e0
Vegard Bønes <vegardb@met.no>
2024-03-20 11:04:27 +0100

    move onpress functionality from component to screen

--- src/screens/no-location.screen.tsx
+++ src/screens/no-location.screen.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { ImageBackground, StyleSheet, View, ScrollView } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import appBackground from '../../assets/appbackground.png';
 import AppBar from '../components/AppBar';
 import LocationRow from '../components/LocationRow';
 import { DISTRICTS } from '../constants/districts.constant';
-import { RootState } from '../store';
+import { AppDispatch, RootState } from '../store';
 import { RootDrawerParamList } from '../common';
+import { setForecast } from '../store/forecast.slice';
+import { setLat, setLon, setName } from '../store/location.slice';
+import { SCREENS } from '../constants/screens.constant';
+import { WeatherForecast } from '../utils/locationforecast';
 
 type ScreenProps = NativeStackScreenProps<RootDrawerParamList, 'NoLocation'>;
 const NoLocationScreen = ({ navigation }: ScreenProps) => {
   const { name } = useSelector((state: RootState) => state.location);
+  const dispatch = useDispatch<AppDispatch>();
+
 
   return (
     <SafeAreaView>
@@ -22,7 +28,21 @@ const NoLocationScreen = ({ navigation }: ScreenProps) => {
           <AppBar location={name} navigation={navigation} />
           <ScrollView contentContainerStyle={styles.container}>
             {
-              DISTRICTS.map((district, idx) => <LocationRow key={idx} district={district} navigation={navigation} />)
+              DISTRICTS.map((district, idx) => 
+              <LocationRow 
+                key={idx} 
+                district={district} 
+                navigation={navigation} 
+                onPress={(forecast: WeatherForecast) => 
+                  () => {
+                    dispatch(setForecast(forecast))
+                    dispatch(setName(district.name));
+                    dispatch(setLat(district.lat));
+                    dispatch(setLon(district.lon));
+                    navigation.navigate(SCREENS.Home);
+                  }
+                }
+              />)
             }
           </ScrollView>
         </ImageBackground>
