24a894082bd59f3ec965c107c93b0a2789654c24
Vegard Bønes <vegardb@met.no>
2024-03-20 14:01:16 +0100

    do not delete old forecast when user searches for a new place - move that functionality to main page

--- src/screens/search.screen.tsx
+++ src/screens/search.screen.tsx
@@ -29,7 +29,7 @@ const SearchScreen = ({ navigation }: ScreenProps) => {
               location={name}
               setLocation={
                 place => {
-                  dispatch(setForecast(undefined))
+                  // No need to reset forecast here.
                   dispatch(setName(place.name));
                   dispatch(setLat(place.lat));
                   dispatch(setLon(place.long));
