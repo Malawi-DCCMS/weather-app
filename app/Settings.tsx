b023449105cac07205d4e3a323208b320e3008a4
Daniel Mwakanema <danielmwakanema95@gmail.com>
2025-04-07 16:09:49 +0200

    refactor(settings): switch dropdown select src

--- app/Settings.tsx
+++ app/Settings.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { ImageBackground, StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Icon, Text } from 'react-native-paper';
 import { useSelector } from 'react-redux';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Dropdown } from 'react-native-paper-dropdown';
+import Dropdown from 'react-native-input-select';
 
 import Alerts from '@/components/Alerts';
 import AppBar from '@/components/AppBar';
@@ -29,13 +29,19 @@ const SettingsScreen = () => {
           <Alerts lat={lat} lon={lon} location={name} />
           <View>
             <Text style={styles.text}>Language</Text>
-            <Dropdown
-              label="Language"
-              placeholder="Select Language"
-              options={options}
-              value={language}
-              onSelect={setLanguage}
-            />
+                            <Dropdown
+                  label="Language"
+                  placeholder="Select a language..."
+                  options={options}
+                  selectedValue={language}
+                  onValueChange={value => setLanguage(value as string)}
+                  primaryColor={'#313131'}
+                  dropdownStyle={styles.settings}
+                  placeholderStyle={{ color: 'white' }}
+                  selectedItemStyle={{ color: 'white' }}
+                  dropdownIconStyle={{ borderColor: 'white', borderWidth: 1, borderRadius: 1000 }}
+                  dropdownIcon={<Icon source={'chevron-down'} color='white' size={24} />}
+                />
           </View>
         </ImageBackground>
       </View>
