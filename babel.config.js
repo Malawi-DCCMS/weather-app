6dbd4908e80d2988fa02c7b4bcc804b5edc9f4ea
Daniel Mwakanema <danielmwakanema95@gmail.com>
2025-08-12 09:30:39 +0200

    fix(app): drop react-native-dotenv
    - remove package from project
    - conflicts with expo and causes app to fail to start

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,6 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(false);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module:react-native-dotenv',
-        {
-          "envName": "APP_ENV",
-          "moduleName": "react-native-dotenv",
-          "path": ".env",
-          "safe": false,
-          "allowUndefined": true
-        }
-      ],
-    ],
   };
 };
